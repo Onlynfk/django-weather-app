b1febbf536f8eed7b1ae46d078ed413816b99779
OnlynfK <onlygod15@hotmail.com>
2020-03-23 22:55:04 +0100

    gitignore working

--- weather_now/weather_now/settings.py
+++ weather_now/weather_now/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from weather_now.secrets import *
+from weather_now.sc.secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
